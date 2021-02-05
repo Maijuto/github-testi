a6e68540b0a9bb1648686864e0f6f62bcaf03610
Maijuto <maiju.toivonen@metropolia.fi>
2021-02-05 10:19:48 +0200

    testi

---
+++ kertotaulu2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laskin</title>
+</head>
+<body>
+    <h1> Tervetuloa, syötä numero ja saat vastaukseksi luvun kertotaulun 1-10! </h1>
+    <form action="kertotaulu2.php" method="POST">
+    <input type="number" name="number">     <br>
+    <input type="submit" name="table" value="laske"> 
+</form>
+<?php
+$num=$_POST['number'];
+
+if($num<=100)
+
+{
+  for ($i=1; $i<=10; $i++)
+  {
+    $mul=$num*$i;
+    echo "$num * $i = $mul<br>";
+   }
+}
+  else
+  {
+  echo "Jokin meni pieleen";
+   }
+
+?>
+</body>
+</html>
